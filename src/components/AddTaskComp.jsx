eaec983ae1003d3418143f92860429aa00a2c294
Gagandeep Singh <gagan.official2k1@gmail.com>
2024-06-18 03:08:35 +0530

    completed todo app with localstorage, only toast msgs remaining to add

---
+++ src/components/AddTaskComp.jsx
@@ -0,0 +1,17 @@
+import CustButton from "./CustButton";
+import { commonRounded } from "../App";
+
+function AddTaskComp({ custButtonProps, inputProps, formOnSubmit }) {
+  return (
+    <form onSubmit={formOnSubmit} className="inputButtonCont flex gap-2">
+      <input
+        {...inputProps}
+        className={`border ${commonRounded} border-gray-400 px-2 ${inputProps.className ?? ""}`}
+        required
+      />
+      <CustButton {...custButtonProps} type="submit">Add</CustButton>
+    </form>
+  );
+}
+
+export default AddTaskComp;
---
+++ src/components/CustButton.jsx
@@ -0,0 +1,15 @@
+import { btnHover, commonRounded } from "../App";
+
+function CustButton(props) {
+  const { className, children } = props;
+  return (
+    <button
+      {...props}
+      className={`bg-blue-600 text-white ${commonRounded} px-3 py-1 ${btnHover} ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default CustButton;
